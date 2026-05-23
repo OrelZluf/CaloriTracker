038d068e6012c89d3a9c94da59e01ccd9c44fd0b
orel zluf <orelz16@gmail.com>
2026-05-24 00:16:24 +0300

    Fix computed import in profile

--- client/src/app/features/profile/profile.ts
+++ client/src/app/features/profile/profile.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, signal, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../core/services/auth.service';
